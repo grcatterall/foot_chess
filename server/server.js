158e3b83a4ebe472f504c5226f29b7295aeffde8
Graham Catterall <grahamcatterall@Grahams-MacBook-Air-2.local>
2023-10-24 12:12:23 +0100

    init

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const http = require('http'),
+      path = require('path'),
+      express = require('express'),
+      handlebars = require('express-handlebars'),
+      socket = require('socket.io');
+
+const config = require('../config');
+
+const myIo = require('./sockets/io'),
+      routes = require('./routes/routes');
+
+const app = express(),
+      server = http.Server(app),
+      io = socket(server);
+
+server.listen(config.port);
+
+games = {};
+
+myIo(io);
+console.log(games);
+
+console.log(`Server listening on port http://localhost:${config.port}`);
+
+const Handlebars = handlebars.create({
+  extname: '.html', 
+  partialsDir: path.join(__dirname, '..', 'front', 'views', 'partials'), 
+  defaultLayout: false,
+  helpers: {}
+});
+app.engine('html', Handlebars.engine);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '..', 'front', 'views'));
+app.use('/public', express.static(path.join(__dirname, '..', 'front', 'public')));
+
+routes(app);
